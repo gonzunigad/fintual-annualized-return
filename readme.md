fb57e9e58628c0858ab7172a37c8ff3d70f16776
Gonzalo De Spírito <gonzunigad@gmail.com>
2021-11-30 00:28:48 -0300

    feat: add more test to check if it works with multiple stocks

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Fintual Task 
+
+👋 Este proyecto busca resolver el siguiente desafío:
+
+    Build a simple Portfolio class that has a collection of Stocks and a "Profit" method that receives 2 dates and returns the profit of the Portfolio between those dates. Assume each Stock has a "Price" method that receives a date and returns its price.
+    Bonus Track: make the Profit method return the "annualized return" of the portfolio between the given dates.
+
+El archivo interesante está en `src/portfolio.js`;
+Hay un método `profit` que devuelve las ganancias entre dos fechas. 
+
+Si quieren probar: 
+```bash
+npm install
+npm test
+```
--- tests/portfolio.test.js
+++ tests/portfolio.test.js
@@ -8,11 +8,32 @@ test('it can calculates profit', () => {
     expect(portfolio.profit(from, to)).toBe(90);
 });
 
+test('it can calculates profit with multiple stocks', () => {
+    const portfolio = new Portfolio;
+    const from = '2020/11/29';
+    const to = '2021/11/29';
+    portfolio.addStock(new Stock('AAPL', {[from]: 10, [to]: 100}))
+    portfolio.addStock(new Stock('TSLA', {[from]: 10, [to]: 100}))
+    expect(portfolio.profit(from, to)).toBe(180);
+});
+
 test('it can calculates annualized return', () => {
     const portfolio = new Portfolio;
     const from = '2020/11/29';
     const to = '2022/06/26';
     portfolio.addStock(new Stock('AAPL', {[from]: 100, [to]: 123.74}))
+
+    //As this example, 23.74% if 575 days would be 14.5 annualized return.
+    // https://www.investopedia.com/terms/a/annualized-total-return.asp
+    expect(parseFloat(portfolio.annualizedReturn(from, to).toFixed(1))).toBe(14.5);
+});
+
+test('it can calculates annualized return with multiple stocks', () => {
+    const portfolio = new Portfolio;
+    const from = '2020/11/29';
+    const to = '2022/06/26';
+    portfolio.addStock(new Stock('AAPL', {[from]: 100, [to]: 123.74}))
+    portfolio.addStock(new Stock('TSLA', {[from]: 1000, [to]: 1237.4}))
 
     //As this example, 23.74% if 575 days would be 14.5 annualized return.
     // https://www.investopedia.com/terms/a/annualized-total-return.asp
